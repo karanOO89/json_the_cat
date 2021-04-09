139e436d34c794ba1424857f8b9e62c67191dc57
karanOO89 <gk.987680@gmail.com>
2021-04-09 00:18:45 +0000

    refactoring the json cat code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,25 +1,23 @@
-const catDes = process.argv.slice(2);
 const request = require("request");
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${catDes}`,
-  function(error, response, body) {
-    const data = JSON.parse(body);
-    // console.log(error);
-    if (response.statusCode !== 200) {
-      throw new Error(`${catDes} not found`);
+const fetchBreedDescription = (breedName, callback) => {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    function(error, response, body) {
+      if (error) {
+        return callback(error, null);
+      }
+      if (response.statusCode !== 200) {
+        return callback(error, null);
+      }
+    
+      const data = JSON.parse(body);
+      // console.log(data);
+      if (data[0]) {
+        return callback(null, data[0].description);
+      } else {
+        return callback(null, "breed not found");
+      }
     }
-    if (response.statusCode === 200 && undefined) {
-      console.log(response.statusCode);
-      throw new Error("no relevant data found");
-    }
-    // if (request.responseJSON === 201) {
-    //     throw new Error('${catDes} not found');
-    //   }
-    // console.error(`error:${catDes} not found`, error);
-    // console.log("statusCode:", response && response.statusCode);
-    // //   console.log('body:', body);
-    //   console.log("typeof body",typeof(body))
-    console.log(data[0].description);
-    // console.log(typeof data);
-  }
-);
+  );
+};
+module.exports = fetchBreedDescription;
